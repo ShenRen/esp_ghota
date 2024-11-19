18021b549aada8717a3da7c6a6c13349e297a4d1
RenShen <ren.shen@qq.com>
2024-11-19 19:10:04 +0800

    add esptool_rename_bin function to rename bin files

--- project_include.cmake
+++ project_include.cmake
@@ -5,19 +5,29 @@
 # The merge_bin command will merge multiple binary files (of any kind) into a single file that can be flashed to a device later. 
 # Any gaps between the input files are padded based on the selected output format.
 # flashed using `idf.py flash`
-function(esptool_merge_bin suffix)
+function(esptool_merge_bin out_name)
     idf_build_get_property(idf_path IDF_PATH)
     set(esptool_py ${PYTHON} ${idf_path}/components/esptool_py/esptool/esptool.py)
 
-    set(image_name ${CMAKE_PROJECT_NAME}_merged_${suffix}.bin)
+    set(image_name ${out_name}.bin)
 
     # Execute esptool merge_bin command; this always executes as there is no way to specify for CMake to watch for
     # contents of the base dir changing.
     add_custom_target(${CMAKE_PROJECT_NAME}_merged_bin ALL
+        DEPENDS gen_project_binary
         COMMAND ${esptool_py} --chip ${CONFIG_IDF_TARGET} merge_bin -o ${image_name} @flash_args
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT "Generating merged binary image: ${image_name}"
         )
-    add_dependencies(${CMAKE_PROJECT_NAME}_merged_bin gen_project_binary)
     add_dependencies(app ${CMAKE_PROJECT_NAME}_merged_bin)
+endfunction()
+
+function(esptool_rename_bin new_name)
+    add_custom_target(${CMAKE_PROJECT_NAME}_rename_bin ALL
+        DEPENDS gen_project_binary
+        COMMAND "${CMAKE_COMMAND}" -E copy "${CMAKE_PROJECT_NAME}.bin" "${new_name}.bin"
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Rename binary image to: ${new_name}.bin"
+        )
+    add_dependencies(app ${CMAKE_PROJECT_NAME}_rename_bin)
 endfunction()
